8e535fea75f826f2cef33ae71479a41f87066d2c
MAtrix <id.matrix.2006@gmail.com>
2023-01-26 17:31:39 +0400

    a little change

--- backend/main/main.js
+++ backend/main/main.js
@@ -5,7 +5,7 @@ const calendarRouter = require('./Routes/routesCalendar');
 const authRouter = require('./Routes/routesAuth');
 const { DateTime } = require('luxon');
 const passport = require('passport')
-
+const address = '192.168.1.25'
 
 server.use(CORSES);
 server.use(URLENCODER);
@@ -14,6 +14,6 @@ require('./Passport/passport')(passport);
 server.use('/calendar', calendarRouter);
 server.use('/auth', authRouter)
 
-server.listen(PORT,'84.201.255.221', () => {
-    console.log(`Server has been started on port ${'84.201.255.221'} ${PORT}`);
+server.listen(PORT,address, () => {
+    console.log(`Server has been started on port ${address} ${PORT}`);
 });
